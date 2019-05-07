876632db136222b0733a27cc0e76897dd61c599d
CJHerrera <wastinglove1@outlook.com>
2019-05-06 21:56:19 -0300

    OTRA VEZ CORRECCIONES

---
+++ VARIABLES/AJUSTAR_RELOJ_1/Module1.vb
@@ -0,0 +1,16 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim a, b As Integer
+        Dim mes As month
+        Dim ano As year
+        Dim resultado As DateTime
+        Console.Write("ingresar un numero del 1 al 31 ")
+        a = Console.ReadLine()
+        Console.Write("ingresar un numero del 0 al 23 ")
+        b = Console.ReadLine()
+
+        resultado = Console.WriteLine("se a formado: " & ano / mes / a / b)
+    End Sub
+
+End Module
