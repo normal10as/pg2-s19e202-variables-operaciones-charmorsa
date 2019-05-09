7ef5d8ad2c9e6f4ef8999ee026b0c4e001e844fd
CJHerrera <wastinglove1@outlook.com>
2019-05-09 13:56:42 -0300

    1.9 agregaado

---
+++ VARIABLES/DIAS/Module1.vb
@@ -0,0 +1,33 @@
+﻿Imports Microsoft.VisualBasic.DateAndTime
+
+Module Module1
+
+    Sub Main()
+        Dim a As Integer
+        Console.Write("igrese un numero de la semana. del 1 al 7    ")
+        a = Console.ReadLine()
+
+        If ((a > 0) And (a < 8)) Then
+            Select Case a
+                Case 1
+                    Console.WriteLine("LUNES")
+                Case 2
+                    Console.WriteLine("MARTES")
+                Case 3
+                    Console.WriteLine("MIERCOLES")
+                Case 4
+                    Console.WriteLine("JUEVES")
+                Case 5
+                    Console.WriteLine("VIERNES")
+                Case 6
+                    Console.WriteLine("SABADO")
+                Case 7
+                    Console.WriteLine("DOMINGO")
+            End Select
+
+        Else
+            Console.WriteLine("el numero ingresado es incorreccto")
+        End If
+    End Sub
+
+End Module
--- VARIABLES/INTERES/Program.vb
+++ VARIABLES/INTERES/Program.vb
@@ -2,7 +2,9 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Dim m, r, t, i As Decimal
+        Dim m, i As Decimal
+        Dim r As Single
+        Dim t As UShort
         Console.WriteLine(" Ingrese el monto: ")
         m = Console.ReadLine()
         Console.WriteLine(" Ingrese el interes anual: ")
@@ -12,6 +14,6 @@ Module Program
 
         i = (m * r * t) / (360 * 100)
         Console.WriteLine("el interes producido es de " & i & " segun los datos ingresados...")
-
+        Console.WriteLine("el interes anual " & m + i)
     End Sub
 End Module
